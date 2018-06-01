843bbcfb4babfddc93d38b946c069f5038c28914
Alexandre Moraes <me@alcmora.es>
2018-06-01 11:03:37 -0300

    :zap: Optmizing maintainability; - Removing complexity from 'readCsv' method; - Deduplicating methods over different tasks; - Adding importer worker on pm2.json script; - Adding csv/**/*.csv on gitignore;

--- services/utils.js
+++ services/utils.js
@@ -1,3 +1,4 @@
+const spawn = require( 'child_process' ).spawn;
 const _ = require( 'lodash' );
 const CSV = require( 'csv-string' );
 
@@ -6,66 +7,90 @@ const CSV = require( 'csv-string' );
  */
 class Utils {
     /**
+     * Spawns a cmd on a different thread
+     *
+     * @param {string} cmd
+     * @param {Array} args
+     * @param {boolean} verbose
+     * @return {Promise}
+     */
+    spawn( cmd, args, verbose = false ){
+        return new Promise( ( resolve, reject ) => {
+            let THREAD;
+            THREAD = spawn( cmd, args );
+            if( verbose ){
+                THREAD.stdout.on( 'data', ( data ) => {
+                    _.forEach( data.toString().split( /(\r?\n)/g ), ( d ) => console.log( d ) );
+                } );
+            }
+            THREAD.on( 'close', ( code ) => {
+                return code ? resolve() : reject();
+            } );
+        } );
+    }
+
+    /**
+     * Checks for the first valid row that matches
+     * the columns specification and stores the position
+     * of each column.
+     *
+     * That way, we can accept CSV's with any formation
+     * (I hope so)
+     *
+     * @param {Array} data The array of given companies extracted from CSV
+     * @param {Object} columns The column rule for matching data
+     * @param {function} cb The callback to execute before finishing it
+     * @return {function}
+     */
+    guessColumnPositions( data, columns, cb ){
+        try{
+            let columnOrder = [];
+            let entity = data.shift();
+            _.forEach( columns, ( prop, col ) => {
+                let match;
+                let regex = prop.hasOwnProperty( 'regex' ) ? prop.regex : prop;
+                match = _.find( entity, ( entry ) => entry.match( regex ) );
+                if( match ){
+                    columnOrder.push( { name: col, required: prop.required,
+                        pos: entity.indexOf( match ),
+                        regex } );
+                }
+                if( !match && prop.required ) return columnOrder = [] && false;
+            } );
+            if( !columnOrder.length ) return this.guessColumnPositions( data, columns, cb );
+            return data.unshift( entity ) && cb( null, columnOrder );
+        }
+        catch( ERR ){
+            return cb( ERR );
+        }
+    };
+
+    /**
      * Parses a CSV from a given CSV File data
      * @param {string} csvString
      * @param {Object} columns a Regular expression collection to match the CSV
      * @return { Promise } Arra<Array> or Array<Object>, depending on the columns parameter
      */
     readCsv( csvString, columns = {} ){
-        return new Promise( ( resolve, reject ) => {
+        return new Promise( async ( resolve, reject ) => {
             let data;
-            let columnOrder = [];
             try{
                 if( _.isEmpty( columns ) ) throw new Error( '`columns` property cannot be empty!' );
                 data = CSV.parse( csvString );
                 if( !data.length ) throw new Error( 'Empty csv' );
-                /**
-                 * Checks for the first valid row that matches
-                 * the columns specification and stores the position
-                 * of each column.
-                 *
-                 * That way, we can accept CSV's with any formation
-                 * (I hope so)
-                 *
-                 * @return {void}
-                 */
-                let guessColumnPositions = () => {
-                    let entity = data.shift();
-                    _.forEach( columns, ( prop, col ) => {
-                        let match;
-                        let regex = prop.hasOwnProperty( 'regex' ) ? prop.regex : prop;
-                        match = _.find( entity,
-                            ( entry ) => entry.match( regex ) );
-                        if( match ){
-                            columnOrder.push( {
-                                name: col,
-                                required: prop.required,
-                                pos: entity.indexOf( match ),
-                                regex,
-                            } );
-                        }
-                        if( !match && prop.required ){
-                            columnOrder = [];
-                            return false;
-                        }
-                    } );
-                    if( !columnOrder.length ) return guessColumnPositions();
-                    data.unshift( entity );
-                };
-
-                guessColumnPositions();
-
-                resolve( data.reduce( ( output, company ) => {
-                    let c = {};
-                    columnOrder.map( ( o, i ) => {
-                        if( ( !company[ o.pos ] && o.required ) ||
-                            ( !company[ o.pos ].match( o.regex ) && o.required )
-                        ) return;
-                        c[ o.name ] = company[ o.pos ];
-                    } );
-                    return Object.keys( c ).length === columnOrder.length ?
-                        [ ...output, c ] : output;
-                }, [] ) );
+                this.guessColumnPositions( data, columns, ( err, columnOrder ) => {
+                    resolve( data.reduce( ( output, company ) => {
+                        let c = {};
+                        columnOrder.map( ( o, i ) => {
+                            if( ( !company[ o.pos ] && o.required ) ||
+                                ( !company[ o.pos ].match( o.regex ) && o.required )
+                            ) return;
+                            c[ o.name ] = company[ o.pos ];
+                        } );
+                        return Object.keys( c ).length === columnOrder.length ?
+                            [ ...output, c ] : output;
+                    }, [] ) );
+                } );
             }
             catch( ERR ){
                 reject( ERR );
--- tasks/bootstrap.js
+++ tasks/bootstrap.js
@@ -1,33 +1,24 @@
 #!/bin/node
 
 const debug = require( 'debug' )( 'API:TASKS:BOOTSTRAP' );
-const spawn = require( 'child_process' ).spawn;
-const _ = require( 'lodash' );
+const utils = require( '../services/utils' );
 const path = require( 'path' );
 
 let bootstrap = () => {
     return new Promise( async ( resolve, reject ) => {
         let mongoose;
         let collections;
-        let THREAD;
         try{
             mongoose = await require( '../services/database' );
             collections = await mongoose.db.db.collections();
             for( let collection of collections ) await collection.remove();
             
-            THREAD = spawn( 'node', [
+            await utils.spawn( 'node', [
                 './tasks/import_from_file.js',
-                `file=${path.join( __dirname, 'initial_data', 'companies.csv' )}`,
+                `file=${path.join( __dirname, 'initial_data', 'q1_catalog.csv' )}`,
             ] );
 
-            THREAD.stdout.on( 'data', ( data ) => {
-                _.forEach( data.toString().split( /(\r?\n)/g ), ( d ) => console.log( d ) );
-            } );
-
-            THREAD.on( 'close', ( code ) => {
-                debug( `Bootstrap task finished with ${code === 0 ? 'success' : 'error'}` );
-                resolve( code );
-            } );
+            resolve();
         }
         catch( ERR ){
             debug( ERR );
